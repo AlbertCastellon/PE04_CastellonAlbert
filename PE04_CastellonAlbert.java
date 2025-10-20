fd5990a82c207fe43e366d1f9ede7dfa6720244d
Pepe Sufragio <albertkstyon@gmail.com>
2025-10-20 16:57:35 +0200

    first commit

---
+++ PE04_CastellonAlbert.java
@@ -0,0 +1,26 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class PE04_CastellonAlbert {
+    boolean lighth1 = false, lighth2 = false, lighth3 = false, lightkitchen = false, lightlivroom = false;
+    static Scanner escaner = new Scanner(System.in);
+    static int mainMenu = 0;
+    static int menuStrate = 0;
+    public static void main(String[] args) {
+        System.out.println("Què vols controlar?");
+        System.out.println("1. Llums");
+        while (menuStrate == 0){
+            System.out.println("Escull una opció");
+            try {
+                mainMenu = escaner.nextInt();
+                menuStrate ++;
+            }catch(InputMismatchException e) {
+                System.out.println("Opció invàlida escull una altra vegada.");
+            }
+            if (mainMenu > 1 || mainMenu < 1){
+                System.out.println("Aquesta opció no esta disponeble encara.");
+            }
+        }
+        
+    }
+}
